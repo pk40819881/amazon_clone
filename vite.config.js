a4ae010c9f3bf77bbc1e3a356254868562c90f77
prateekSharingan <pk.4081988@gmail.com>
2026-06-18 01:34:53 +0530

    project

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/react-vue'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'amazon_clone',
 })
